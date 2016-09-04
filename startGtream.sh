9dd7c59697be3ea376d3be965c63f76544130b7a
´schlank´ <phil@philipleder.com>
2016-09-04 18:53:25 +0000

    updates man wtf

--- startGtream.sh
+++ startGtream.sh
@@ -6,9 +6,9 @@
 _IP=$(hostname -I) || true
 if [ "$_IP" ]; then
    printf "My IP Address is %s\n" $_IP
+   espeak "IP Address is ${_IP}"
    UBER_HOST=$_IP
    UBER_PORT=5000
    echo $UBER_HOST
-
    raspivid -fps 25 -h 600 -w 800 -vf -hf -n -t 0 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=$UBER_HOST port=$UBER_PORT
 fi
