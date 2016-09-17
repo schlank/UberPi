562cfd66da1b84216e3eac98bad967035a66a1a7
Starla Anderson <phil@philipleder.com>
2016-09-17 09:57:56 -0700

    ggogoog

--- startGtream.sh
+++ startGtream.sh
@@ -9,6 +9,7 @@ if [ "$_IP" ]; then
    espeak "IP Address is ${_IP}"
    UBER_HOST=$_IP
    UBER_PORT=5000
+   espeak "Port... ${UBER_PORT}"
    echo $UBER_HOST
    raspivid -fps 25 -h 600 -w 800 -vf -hf -n -t 0 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=$UBER_HOST port=$UBER_PORT
 fi
