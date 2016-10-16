976f9c43788b0903e754e5927ea2cc1f2a2f9831
Starla Anderson <phil@philipleder.com>
2016-10-15 21:18:40 -0700

    Updates to everything

--- robot/ControlsHandler.py
+++ robot/ControlsHandler.py
@@ -12,4 +12,4 @@ class ControlsHandler(socketserver.BaseRequestHandler):
 
         request, socket = self.request  # Read who spoke to us and what they said
         robot_wheels = pickle.loads(request)
-        print(robot_wheels)
+        print(robot_wheels.hasCommands())
