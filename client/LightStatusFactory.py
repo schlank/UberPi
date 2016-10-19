097c8235a7586b989f0bb670b1ee9e12271b23eb
Starla Anderson <phil@philipleder.com>
2016-10-18 23:37:30 -0700

    Buttons on wheel

--- client/LightStatusFactory.py
+++ client/LightStatusFactory.py
@@ -4,7 +4,6 @@ from client.LightStatus import LightStatus
 class LightStatusFactory:
     @staticmethod
     def create_light_status_from_wheel(racing_wheel):
-        print(racing_wheel)
         if racing_wheel.leftButton.on:
             return LightStatus(True)
         elif racing_wheel.rightButton.on:
