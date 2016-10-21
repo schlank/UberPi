f4350fe48211e39613efdd5d56079f4e05e23fcc
Starla Anderson <phil@philipleder.com>
2016-10-20 22:25:45 -0700

    Buttons on wheel

--- client/ServoStatusFactory.py
+++ client/ServoStatusFactory.py
@@ -6,12 +6,14 @@ class ServoStatusFactory:
     @staticmethod
     def create_servo_status_w_racing_wheel(racing_wheel):
         direction = None
-        if racing_wheel.leftShiftButton:
+        if racing_wheel.leftShiftButton.on:
             direction = "DOWN"
-        elif racing_wheel.rightShiftButton:
+        elif racing_wheel.rightShiftButton.on:
             direction = "UP"
+
         if direction is not None:
-            print(racing_wheel)
+            print(racing_wheel.leftShiftButton.on)
+            print(racing_wheel.rightShiftButton.on)
             return ServoStatus(direction)
         return None
 
